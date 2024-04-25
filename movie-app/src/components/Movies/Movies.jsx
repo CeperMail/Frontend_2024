9b8e22f62d7efa71d401438dbd171dd15f663dbd
CeperMail <arifinilham6970@gmail.com>
2024-04-25 10:21:09 +0700

    tugas pertemuan6

--- movie-app/src/components/Movies/Movies.jsx
+++ movie-app/src/components/Movies/Movies.jsx
@@ -1,37 +1,33 @@
-import styles from "./Movies.module.css"
+import Movie from "../Movie/Movie.jsx";
+import styles from "./Movies.module.css";
+import data from "../../utils/constants/data.js"
+import { useState } from "react";
 
 function Movies (){
+
+    const [movies, setMovies] = useState(data);
+
+    function handleClick(){
+        const newMovie = {
+            id: "tt17907586",
+            title: "Siksa Kubur",
+            year: "2024",
+            type: "Movie",
+            poster: "https://upload.wikimedia.org/wikipedia/id/b/bf/Poster_Siksa_Kubur.jpg"
+        }
+        setMovies([...movies,newMovie])
+    }
+
     return (
         <div className={styles.container}> 
             <section className={styles.movies}>
                 <h2 className={styles.movies__title}>Latest Movies</h2>
                 <div className={styles.movie__container}>
-                    <div className={styles.movie}>
-                        <img className={styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className={styles.movie__title}>Movie Title</h3>
-                        <p className={styles.movie__date}>Date Title</p>
-                    </div>
-                    <div className={styles.movie}>
-                        <img className={styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className={styles.movie__title}>Movie Title</h3>
-                        <p className={styles.movie__date}>Date Title</p>
-                    </div>
-                    <div className={styles.movie}>
-                        <img className={styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className={styles.movie__title}>Movie Title</h3>
-                        <p className={styles.movie__date}>Date Title</p>
-                    </div>
-                    <div className={styles.movie}>
-                        <img className={styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className={styles.movie__title}>Movie Title</h3>
-                        <p className={styles.movie__date}>Date Title</p>
-                    </div>
-                    <div className={styles.movie}>
-                        <img className={styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className={styles.movie__title}>Movie Title</h3>
-                        <p className={styles.movie__date}>Date Title</p>
-                    </div>
+                    {
+                        movies.map((movie) => {return <Movie key={movie.id} movie={movie}/>})
+                    }
                 </div>
+                <button onClick={handleClick} className={styles.more_btn}>Add Movie</button>
             </section>
         </div>
     )
